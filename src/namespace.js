d592afed9316b2c2228a8e90cd52fa10f8af6694
tg0825 <tg0825@backpack.kr>
2018-01-20 14:55:45 +0900

    tree

---
+++ src/namespace.js
@@ -0,0 +1,22 @@
+// namespace.js
+if (!naver) {
+    /**
+     * @namespace
+     */
+     var naver = {};
+}
+
+/**
+ * @namespace
+ */
+naver.model = naver.modal || {};
+
+/**
+ * @namespace
+ */
+naver.collection = naver.collection || {};
+
+/**
+ * @namespace
+ */
+naver.view = naver.view || {};
---
+++ src/naver.collection.Organizations.js
@@ -0,0 +1,66 @@
+// naver.collection.Organizations.js
+/**
+ * @param {string} sApiUrl 조직도 API 주소
+ * @constructor
+ */
+naver.collection.Organizations = function (sApiUrl) {
+    this._sApiUrl = sApiUrl;
+    this._aComposite = [];
+    this._requestOrganizations();
+};
+
+naver.collection.Organizations.prototype = {
+    /**
+     * 조직을 트리 구조로 조합한다.
+     * @param {Array.<naver.model.Organization>} aOrganizations
+     * @private
+     */
+    _composeOrganizations: function (aOrganizations) {
+        var oParent = null;
+        var oSelf = this;
+
+        _.each(aOrganizations, function (oOrganization) {
+            if (oOrganization.nParentId === -1) {
+                oSelf._aComposite.push(oOrganization);
+            } else {
+                oParent = _.findWhere(aOrganizations, {nId: oOrganization.nParentId});
+                oParent.appendChild(oOrganization);
+            }
+        });
+    },
+
+    /**
+     * 조직 리스트를 객체화한다.
+     * @param {Array.<OrganizationDataSet>} aListData
+     * @returns {Array}
+     * @private
+     */
+    _createOrganizations: function (aListData) {
+        var aOrganizations = [];
+        _.each(aListData, function (htDataSet) {
+            aOrganizations.push(new naver.model.Organizations(htDataSet));
+        });
+
+        return aOrganizations;
+    },
+
+    /**
+     * 조직 리스트를 요청한다.
+     * @private
+     */
+    _requestOrganizations: function () {
+        // var oSelf = this;
+
+        $.ajax({
+            url: this._sApiUrl,
+            async: false,
+        // }).then(function(htListData) {
+        //     var aOrganizations = oSelf._createOrganizations(htListData)
+        //     oSelf._composeOrganizations(aOrganizations);
+        //     console.log(oSelf._aComposite);
+        // });
+
+        }).then($.proxy(this._createOrganizations, this))
+        .then($.proxy(this._composeOrganizations, this));
+    }
+};
